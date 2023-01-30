43cf9fcfbf771c41dcb2b9ac50b987080ecaebbc
Nigma Silmi <sn.silmi@gmail.com>
2023-01-30 16:01:05 -0300

    storybook install and baby steps with first component and examples

---
+++ src/components/Label.tsx
@@ -0,0 +1,7 @@
+import "./Label.css";
+
+const Label = () => {
+  return <span className="label">Hola mundirijillo</span>;
+};
+
+export default Label;
---
+++ src/stories/components/Label.stories.tsx
@@ -0,0 +1,12 @@
+import Label from "../../components/Label";
+
+export default {
+  title: "Example/Label",
+  component: Label,
+};
+
+const Template = () => <Label />;
+
+export const Basic = Template.bind({});
+export const AllCaps = Template.bind({});
+export const Secondary = Template.bind({});
